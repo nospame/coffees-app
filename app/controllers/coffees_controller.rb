aa972e5fa011995bb31f26742d81b3840ea465c9
nospame <mapson.evan@gmail.com>
2022-04-26 11:47:44 -0700

    add coffees new page with create action

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -4,8 +4,24 @@ class CoffeesController < ApplicationController
     render "coffees/index"
   end
 
+  def new
+    render "coffees/new"
+  end
+
   def show
     @coffee = Coffee.find_by(id: params[:id])
     render "coffees/show"
+  end
+
+  def create
+    coffee = Coffee.new(
+      name: params[:name],
+      origin: params[:origin],
+      price: params[:price],
+      roast: params[:roast],
+      image: params[:image]
+    )
+    coffee.save
+    redirect_to "/coffees/#{coffee.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/coffees" => "coffees#index"
+  get "/coffees/new" => "coffees#new"
   get "/coffees/:id" => "coffees#show"
+  post "/coffees" => "coffees#create"
 end
